1e4b38c6c85b92b35e054f383ea71e26cf5f9db1
alex-hh <alex.hhooker@gmail.com>
2023-05-21 16:13:55 +0100

    make package

---
+++ setup.py
@@ -0,0 +1,10 @@
+# https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
+from setuptools import setup
+
+
+setup(
+    name="progen",
+    version="0.1.0",
+    # list folders, not files
+    packages=["progen2"],
+)
